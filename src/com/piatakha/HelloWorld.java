eff366116cd7552ee9570c61581974e329a203aa
Andrii Piatakha <andrey.pyatakha@gmail.com>
2020-02-08 14:17:47 +0200

    init commit

---
+++ src/com/piatakha/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.piatakha;
+
+public class HelloWorld {
+// please don't remove this 
+	
+/*
+ * Please
+ * ohhh
+ * please
+ * don't remove this
+ */
+	
+/**
+ * Some java doc
+ * @param args
+ */
+	public static void main(String[] args) {
+		System.out.println("Viva Java" + " " + args[0]);
+	}
+}
